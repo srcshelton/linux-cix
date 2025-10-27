69ce7ec3badf388de250c16695241b0e1696bd9d
Stuart Shelton <stuart@shelton.me>
2025-10-27 16:31:27 +0000

    Apply Gentoo patch 2990_libbpf-v2-workaround-Wmaybe-uninitialized-false-pos.patch
    Signed-off-by: Stuart Shelton <stuart@shelton.me>

--- tools/lib/bpf/elf.c
+++ tools/lib/bpf/elf.c
@@ -252,6 +252,11 @@ out:
 	return ret;
 }
 
+#if !defined(__clang__)
+#pragma GCC diagnostic push
+/* https://gcc.gnu.org/PR114952 */
+#pragma GCC diagnostic ignored "-Wmaybe-uninitialized"
+#endif
 /* Find offset of function name in ELF object specified by path. "name" matches
  * symbol name or name@@LIB for library functions.
  */
@@ -267,6 +272,9 @@ long elf_find_func_offset_from_file(const char *binary_path, const char *name)
 	elf_close(&elf_fd);
 	return ret;
 }
+#if !defined(__clang__)
+#pragma GCC diagnostic pop
+#endif
 
 struct symbol {
 	const char *name;
